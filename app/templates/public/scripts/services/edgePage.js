f43af23825b20971988c0ee93b62278f25b304e6
Ron Edgecomb <ron.edgecomb@gmail.com>
2013-07-10 20:46:15 -0400

    Small formatting updates

--- app/templates/public/scripts/services/edgePage.js
+++ app/templates/public/scripts/services/edgePage.js
@@ -1,28 +1,27 @@
 'use strict';
 
-angular.module('edge.app.services')
-    .factory('edgePage', function ($document, $rootScope) {
-        var edgePage = {};
+angular.module('edge.app.services').factory('edgePage', function ($document, $rootScope) {
+    var edgePage = {};
 
-        edgePage.getTitle = function () {
-            return ($rootScope.page && $rootScope.page.title) ? $rootScope.page.title : $document.title;
-        };
+    edgePage.getTitle = function () {
+        return ($rootScope.page && $rootScope.page.title) ? $rootScope.page.title : $document.title;
+    };
 
-        edgePage.setPageTitle = function (title) {
-            $rootScope.page = $rootScope.page || {};
-            $rootScope.page.title = title;
-            return title;
-        };
+    edgePage.setPageTitle = function (title) {
+        $rootScope.page = $rootScope.page || {};
+        $rootScope.page.title = title;
+        return title;
+    };
 
-        edgePage.getBodyClass = function () {
-            return ($rootScope.page && $rootScope.page.bodyClass) ? $rootScope.page.bodyClass : '';
-        };
+    edgePage.getBodyClass = function () {
+        return ($rootScope.page && $rootScope.page.bodyClass) ? $rootScope.page.bodyClass : '';
+    };
 
-        edgePage.setBodyClass = function (className) {
-            $rootScope.page = $rootScope.page || {};
-            $rootScope.page.bodyClass = className;
-            return className;
-        };
+    edgePage.setBodyClass = function (className) {
+        $rootScope.page = $rootScope.page || {};
+        $rootScope.page.bodyClass = className;
+        return className;
+    };
 
-        return edgePage;
-    });
+    return edgePage;
+});
