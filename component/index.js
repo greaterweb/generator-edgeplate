769fcab77c2e71ea5102bd268d32879accf3e2dd
Ron Edgecomb <ron.edgecomb@gmail.com>
2013-09-01 22:56:05 -0400

    Add checkbox prompts to component sub generator

--- component/index.js
+++ component/index.js
@@ -6,7 +6,7 @@ var ComponentGenerator = module.exports = function ComponentGenerator(args, opti
     // By calling `NamedBase` here, we get the argument to the subgenerator call
     // as `this.name`.
     yeoman.generators.NamedBase.apply(this, arguments);
-    this.componentName = (this._.camelize(this._.slugify(this.name))).toLowerCase() || 'edge';
+    this.componentName = this._.camelize(this._.slugify(this.name)) || 'edge';
 };
 
 util.inherits(ComponentGenerator, yeoman.generators.NamedBase);
@@ -15,20 +15,27 @@ ComponentGenerator.prototype.askFor = function askFor() {
     var cb = this.async();
 
     var prompts = [{
-        type: 'confirm',
-        name: 'hasSCSS',
-        message: 'Do you wish to include a SCSS file with your component?',
-        default: true
-    },{
-        type: 'confirm',
-        name: 'hasJS',
-        message: 'Do you wish to include a JavaScript file with your component?',
-        default: true
+        type: 'checkbox',
+        name: 'features',
+        message: 'Select the supporting files your component needs:',
+        choices: [{
+            name: 'SCSS File',
+            value: 'hasSCSS',
+            checked: true
+        }, {
+            name: 'JS File',
+            value: 'hasJS',
+            checked: true
+        }]
     }];
 
-    this.prompt(prompts, function (props) {
-        this.hasSCSS = props.hasSCSS;
-        this.hasJS = props.hasJS;
+    this.prompt(prompts, function (answers) {
+        var features = answers.features;
+
+        function hasFeature(feat) { return features.indexOf(feat) !== -1; }
+
+        this.hasSCSS = hasFeature('hasSCSS');
+        this.hasJS = hasFeature('hasJS');
 
         cb();
     }.bind(this));
