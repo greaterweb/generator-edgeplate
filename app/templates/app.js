adb0cf496a3a10c7e0e25c1abc1636ff5404f65c
Ron Edgecomb <ron.edgecomb@gmail.com>
2013-09-02 23:36:47 -0400

    Add edgeplate-express-helpers package to project, update app.js for appropriate usage

--- app/templates/app.js
+++ app/templates/app.js
@@ -1,14 +1,16 @@
 'use strict';
 
-// sample express server configuration
 var fs = require('fs'),
     path = require('path'),
     http = require('http'),
+    nopt = require('nopt'),
     express = require('express'),
-    app = express(),
-    server = http.createServer(app),
-    socketio = require('socket.io').listen(server),
-    nopt = require('nopt');
+    ExpressHelpers = require('edgeplate-express-helpers');
+
+var app = express(),
+    server = http.createServer(app);
+
+var expressHelper, socketio, baseURL;
 
 var options = nopt({
     hostname: String,
@@ -20,42 +22,73 @@ var options = nopt({
     baseurl: ['--baseurl']
 }, process.argv, 2);
 
-var port = options.port || 3000,
-    hostname = options.hostname || 'localhost',
+var port = process.env.PORT || options.port || 3000,
+    hostname = options.hostname || null,
     baseURL = options.baseurl || '/',
     appPath = path.resolve(__dirname + '/public');
 
-app.set('port', port);
-app.use(express.favicon());
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.errorHandler());
-app.use(express.logger('dev'));
+exports.configure = function() {
 
-app.use(function renderCompiledJade (req, res, next) {
-    if (req.url.split('.').length === 1) {
-        var name = req.url,
-            view = appPath + name.replace(baseURL, '/');
+    socketio.sockets.on('connection', function (socket) {
 
-        if (fs.existsSync(view + '.jade') || fs.existsSync(view + '.html')) {
-            // for local grunt express server we server temp and public as static
-            // it will attempt to find view + '.html' in both of these directories
-            //
-            // for production it will look only in public
-            req.url += '.html';
-        }
+    });
+
+    expressHelper.registerConfig([baseURL, express.static(appPath)], 30);
+    expressHelper.registerConfig([baseURL, express.directory(appPath)], 30);
+
+    expressHelper.registerRoutesByFile(__dirname + '/routes.js', baseURL);
+};
+
+exports.app = function (options) {
+    socketio = options.socketio || require('socket.io').listen(server);
+    baseURL = options.baseURL || baseURL;
+
+    exports.expressHelper = expressHelper = options.expressHelper || new ExpressHelpers({ app: app, server: server, socketio: socketio });
+
+    exports.configure();
+
+    return app;
+};
+
+if (require.main === module) {
+    socketio = require('socket.io').listen(server);
+    exports.expressHelper = expressHelper = new ExpressHelpers({ app: app, server: server, socketio: socketio });
+
+    // TODO: need to find a better way to do this...
+    expressHelper.registerConfig([baseURL, express.static(__dirname + '/../.tmp')], 30);
+
+    // development only
+    app.configure('development', function () {
+        app.set('port', port);
+        app.use(express.logger('dev'));
+    });
+
+    // production only
+    app.configure('production', function () {
+        app.set('port', 80);
+        app.use(express.compress());
+    });
+
+    expressHelper.registerConfig(express.favicon());
+    expressHelper.registerConfig(express.bodyParser());
+    expressHelper.registerConfig(express.methodOverride());
+    expressHelper.registerConfig(express.cookieParser('wk-cook-ftw'));
+    expressHelper.registerConfig(express.session());
+    expressHelper.registerConfig(app.router, 20);
+
+    exports.configure();
+    expressHelper.applyConfig();
+    expressHelper.applyRoutes();
+
+    // server.listen(port, hostname, function () {
+    // TODO: sort out how to make the hostname work both locally and with UI Express Server
+    if (hostname) {
+        server.listen(port, hostname, function () {
+            console.log('Express server started on port ' + port);
+        });
+    } else {
+        server.listen(port, function () {
+            console.log('Express server started on port ' + port);
+        });
     }
-    next();
-});
-
-app.use(baseURL, express.static(path.resolve(__dirname + '/../.tmp')));
-app.use(baseURL, express.static(appPath));
-
-socketio.sockets.on('connection', function (socket) {
-    console.log('yo mama');
-});
-
-server.listen(port, hostname, function () {
-    console.log('Express server started on port ' + port);
-});
+}
