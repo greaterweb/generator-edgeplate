1d846fc6673ad7b8a839709b65d491af04445fbb
Ron Edgecomb <ron.edgecomb@gmail.com>
2013-09-26 21:38:55 -0400

    Fix dependency and naming issues

--- page/templates/script.js
+++ page/templates/script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-angular.module('edge.app.controllers').controller('<%= controllerName %>Controller', function (edgePage) {
-    edgePage.setPageTitle('<%= appTitle %> &raquo; <%= _.capitalize(name) %>');
-    edgePage.setBodyClass('page-<%= _.ltrim(_.dasherize(controllerName), '-') %>');
+angular.module('edge.app.controllers').controller('<%= controllerName %>Controller', function () {
+    this.info = '<%= appTitle %> &raquo; <%= _.capitalize(name) %>';
 });
