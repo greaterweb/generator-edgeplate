33d6dbc01e679cc2ed21663c85c269d36b737b49
Ron Edgecomb <ron.edgecomb@gmail.com>
2014-04-26 14:19:03 -0400

    Updates to navbar for responsive support and toggling

--- app/templates/public/controllers/App/AppController.js
+++ app/templates/public/controllers/App/AppController.js
@@ -1,5 +1,27 @@
 'use strict';
 
 angular.module('edge.app.controllers').controller('AppController', function (edgePage) {
-    this.edgePage = edgePage;
+    var app = this;
+    app.edgePage = edgePage;
+
+    $scope.$watch(function() {
+        app.showNavbar = edgePage.isNavbarVisible();
+        if (app.showNavbar) {
+            edgePage.addHtmlClass('has-app-header');
+        } else {
+            edgePage.removeHtmlClass('has-app-header');
+        }
+
+        app.showFooter = edgePage.isFooterVisible();
+        if (app.showFooter) {
+            edgePage.addHtmlClass('has-app-footer');
+        } else {
+            edgePage.removeHtmlClass('has-app-footer');
+        }
+    });
+
+    app.collapseNavbar = true;
+    app.toggleNavbar = function () {
+        app.collapseNavbar = !app.collapseNavbar;
+    };
 });
