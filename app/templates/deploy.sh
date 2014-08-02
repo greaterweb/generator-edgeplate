ba5623b8f1f5dec80dbcc4167cdf9b02335e0f49
Ron Edgecomb <ron.edgecomb@gmail.com>
2014-08-02 13:15:28 -0400

    Improvements to the deploy script  - check for presence of .config.sh  - run gulp config.sh if config missing

--- app/templates/deploy.sh
+++ app/templates/deploy.sh
@@ -5,7 +5,22 @@
 # @usage: ./deploy.sh {dev|www}
 
 #get target directory and port numbers from config file - created by grunt build and package.json values
-source ./.config.sh
+CONFIG=./.config.sh
+
+if ! [ -e $CONFIG ]
+then
+echo "$(tput setaf 1)>>>$(tput sgr0) Missing $(tput setaf 3)" $CONFIG "$(tput sgr0) file. Let's generate one using $(tput setaf 2)gulp config.sh$(tput sgr0)"
+gulp config.sh
+if ! [ -e $CONFIG ]
+then
+    echo "$(tput setaf 1)Something went wrong :-($(tput sgr0)"
+    exit 1
+else
+    echo "Attempting to deploy project"
+fi
+fi
+
+source $CONFIG
 
 case "$1" in
 www)
@@ -15,13 +30,13 @@ dev)
     PORT=$DEV_PORT;
 ;;
 *)
-    echo "Usage: $0 {dev|www}";
+    echo "$(tput setaf 1)>>>$(tput sgr0) Usage: $(tput setaf 2)$0 {dev|www}$(tput sgr0)";
     exit 1
 esac
 DST_PATH="/www/$DIR/$1"
 
 #stop the node service
-ssh $HOST "mkdir -pv $DST_PATH; /usr/local/bin/node-ctrl.sh $DST_PATH/app.js $PORT stop; exit" ;
+ssh $HOST "mkdir -pv $DST_PATH; /usr/local/bin/node-ctrl.sh $DST_PATH/server/server.js $PORT stop; exit" ;
 
 #rsync
 rsync -avz --delete-excluded --exclude-from=.excludes dist/$1/ -e ssh $HOST:$DST_PATH ;
