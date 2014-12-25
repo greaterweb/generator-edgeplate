8d6d88a9571dd7a7e58f3cc50f96fabcaeffb358
Joe K <joe@dailyraisin.com>
2014-12-25 09:01:38 -0500

    copy scss-lint.yml

--- app/index.js
+++ app/index.js
@@ -258,6 +258,7 @@ var EdgeplateGenerator = yeoman.generators.Base.extend({
         this.src.copy('editorconfig', '.editorconfig');
         this.src.copy('jshintrc', '.jshintrc');
         this.src.copy('gitignore', '.gitignore');
+        this.src.copy('scss-lint.yml', '.scss-lint.yml');
     },
     layoutFiles: function layoutFiles() {
         this.template('app/index.jade', 'app/index.jade');
