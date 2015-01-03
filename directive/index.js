07eafee2c06888357f5fc9e27f5a0609d57d679b
Joe K <joe@dailyraisin.com>
2015-01-03 14:13:38 -0500

    jshinted directive/index.js

--- directive/index.js
+++ directive/index.js
@@ -3,7 +3,7 @@ var util = require('util');
 var yeoman = require('yeoman-generator');
 var angularUtils = require('../util.js');
 
-var DirectiveGenerator = module.exports = function DirectiveGenerator(args, options, config) {
+var DirectiveGenerator = module.exports = function DirectiveGenerator() { //function DirectiveGenerator(args, options, config) {
     // By calling `NamedBase` here, we get the argument to the subgenerator call
     // as `this.name`.
     yeoman.generators.NamedBase.apply(this, arguments);
